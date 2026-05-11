f4f84ae640b278de1cd73ffff4430e354ac4a163
unknown <ilerimoritiwon78@gmail.com>
2026-05-11 17:33:17 +0100

    gradecalculator

--- gradecalculator.js
+++ gradecalculator.js
@@ -1,3 +1,26 @@
 export function getgrade(score){
-    if ()
+    if (score < 0 || score > 100) {
+        return "invalid score";
+    }
+    if (typeof score !== "number" || isNaN(score)){
+        return "invalid score:score is not a number";
+    }
+    else if (score >= 75){
+        return "A";
+    }
+    else if (score >= 60){
+        return "B";
+    }
+   else if (score >= 50){
+        return "C";
+    }
+    else if (score >= 40){
+        return "D";
+    }
+   else if (score >= 30){
+        return "E";
+    }else{
+        return "F";
+    }
+
 }
